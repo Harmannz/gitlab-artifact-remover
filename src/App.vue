b0268ca172b78b9fe33b42e458debe53a8b73de0
Harman Singh <harman.singh@hotmail.co.nz>
2019-01-04 03:19:09 +1300

    Complete gitlab artifact remover POC
    * Perform field validation
    * Use Cayman theme
    * Add proper UX responses
    * Add Brief text
    * Perform request in parallel
    
    TODO:
    * Circle-CI
    * Make it scalable. i.e. Shouldn't crash when delete many artifacts.
    
    IDEAS:
    * Perform batch processing when deleting many artifacts in parallel

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
   <div id="app">
-    <vue-progress-bar></vue-progress-bar>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <RemoveForm/>
+    <section class="page-header">
+      <h1 class="project-name">Gitlab Artifact Remover</h1>
+      <h2 class="project-tagline">A hack to remove gitlab artifacts</h2>
+      <RemoveForm/>
+    </section>
+    <section class="main-content">
+      <p><a href="https://docs.gitlab.com/ce/api/jobs.html#erase-a-job" target="_blank">Gitlab REST API</a> allows an individual artifact to be deleted. However, it is tedious to delete many artifacts one by one. So I created this application to delete a range of gitlab artifacts.</p>
+      <hr>
+      <p>This page is inspired by <a href="https://blog.fgribreau.com/2018/01/remove-gitlab-ci-artifacts-in-batch.html" target="_blank">this post from Francois-Guillaume Ribreau.</a> </p>
+      <p><a href="https://github.com/Harmannz/gitlab-artifact-remover" target="_blank"><strong>The code can be found here.</strong></a></p>
+
+      <footer class="site-footer">
+        <span class="site-footer-owner"><a href="https://github.com/harmannz">Harman Singh</a>.</span>
+        <span class="site-footer-credits">This page uses <a href="https://github.com/jasonlong/cayman-theme">Cayman</a> theme.</span>
+      </footer>
+    </section>
   </div>
 </template>
 
@@ -19,5 +32,8 @@ export default {
 
 <style lang="scss">
 #app {
+  $header-bg-color: #1D2B64 !default;
+  $header-bg-color-secondary: #F8CDDA !default;
+  @import "styles/external/cayman.scss";
 }
 </style>
--- src/components/RemoveForm.vue
+++ src/components/RemoveForm.vue
@@ -2,21 +2,30 @@
   <div class="removeForm">
     <template>
       <form id="FormValidation" class="md-layout md-alignment-center-center"
-            novalidate @submit.prevent="validateFields">
+        v-bind:class="{ invalidFormValidation: error}"
+        novalidate @submit.prevent="validateFields">
         <md-card md-with-hover class="md-layout-item md-size-66">
 
           <md-card-content>
             <div class="md-layout md-gutter">
               <div class="md-layout-item md-small-size-100">
+                <md-field :class="getValidationClass('projectId')">
+                  <label for="project-id">Project Id</label>
+                  <md-input name="project-id" type="number" id="project-id"
+                            v-model.number="form.projectId" :disabled="deleting" />
+                  <span class="md-error" v-if="!$v.form.projectId.required">
+                    The project id is required
+                  </span>
+                </md-field>
+              </div>
+
+              <div class="md-layout-item md-small-size-100">
                 <md-field :class="getValidationClass('startId')">
                   <label for="start-id">Start Id</label>
                   <md-input name="start-id" type="number" id="start-id"
-                            v-model="form.startId" :disabled="deleting" />
+                            v-model.number="form.startId" :disabled="deleting" />
                   <span class="md-error" v-if="!$v.form.startId.required">
                     The start id is required
-                  </span>
-                  <span class="md-error" v-else-if="!$v.form.startId.minlength">
-                    Invalid start id
                   </span>
                 </md-field>
               </div>
@@ -25,12 +34,12 @@
                 <md-field :class="getValidationClass('endId')">
                   <label for="end-id" >End Id</label>
                   <md-input name="end-id" type="number" id="end-id"
-                            v-model="form.endId" :disabled="deleting" />
+                            v-model.number="form.endId" :disabled="deleting" />
                   <span class="md-error" v-if="!$v.form.endId.required">
                     The end id is required
                   </span>
-                  <span class="md-error" v-else-if="!$v.form.endId.minlength">
-                    Invalid end id
+                  <span class="md-error" v-else-if="!$v.form.endId.endIdGreaterThanOrEqualStartId">
+                    Must be greater or equal to start id
                   </span>
                 </md-field>
               </div>
@@ -59,16 +68,22 @@
             </md-field>
           </md-card-content>
 
-          <md-progress-bar md-mode="indeterminate" v-if="deleting" />
+          <md-progress-bar v-if="performingAction"
+            v-bind:class="{ 'md-accent': error, 'md-primary': success}"
+            :md-mode="deleting ? 'indeterminate' : 'determinate'"
+            :md-value="100" />
 
           <md-card-actions>
-            <md-button type="submit" class="md-primary" :disabled="deleting">
+            <md-button type="submit" class="md-raised md-primary" :disabled="deleting">
               Delete Artifacts
             </md-button>
           </md-card-actions>
         </md-card>
-        <md-snackbar :md-active.sync="artifactsDeleted">
-          The artifacts {{ form.startId }} was saved with success!
+        <md-snackbar :md-active.sync="success" md-position="left" :md-duration="10000">
+          The artifacts {{ form.startId }} to {{ form.endId }} were deleted!!
+        </md-snackbar>
+        <md-snackbar :md-active.sync="error" md-position="left" :md-duration="15000">
+          There was a problem deleting artifacts. Please retry.
         </md-snackbar>
       </form>
     </template>
@@ -80,7 +95,11 @@
 
 <script>
 import { validationMixin } from 'vuelidate';
-import { required } from 'vuelidate/lib/validators';
+import { numeric, required } from 'vuelidate/lib/validators';
+
+const axios = require('axios');
+
+const endIdGreaterThanOrEqualStartId = (value, vm) => vm.startId <= value;
 
 export default {
   components: {},
@@ -94,15 +113,27 @@ export default {
       startId: null,
       endId: null,
     },
-    artifactsDeleted: false,
     deleting: false,
+    success: false,
+    error: false,
   }),
+  computed: {
+    performingAction: function performingAction() {
+      return this.deleting || this.success || this.error;
+    },
+  },
   validations: {
     form: {
       startId: {
         required,
+        numeric,
       },
       endId: {
+        required,
+        numeric,
+        endIdGreaterThanOrEqualStartId,
+      },
+      projectId: {
         required,
       },
       token: {
@@ -126,25 +157,45 @@ export default {
     },
     clearForm() {
       this.$v.$reset();
-      this.form.startId = null;
-      this.form.endId = null;
-      this.form.token = null;
-      this.form.baseUrl = null;
+    },
+    buildRequest(jobId) {
+      return axios.post(
+        `https://${this.form.baseUrl}/api/v4/projects/${this.form.projectId}/jobs/${jobId}/erase`,
+        {},
+        {
+          headers: {
+            'PRIVATE-TOKEN': this.form.token,
+          },
+        },
+      );
     },
     deleteArtifacts() {
+      // turn this into batch processing to sequentially handle large number of requests.
       this.deleting = true;
-      // this.$Progress.start();
-      // this.$Progress.finish();
-      this.clearForm();
-      this.artifactsDeleted = true;
-      window.setTimeout(() => {
-        this.deleting = false;
-      }, 5000);
-      // https://stackoverflow.com/questions/45403463/progress-bar-with-vue-and-axios
+      this.error = false;
+      this.success = false;
+      // build requests array
+      const requests = [];
+      for (let i = this.form.startId; i <= this.form.endId; i += 1) {
+        requests.push(this.buildRequest(i));
+      }
+
+      // execute requests in parallel
+      axios.all(requests)
+        .then(axios.spread(() => {
+          // do something with both responses
+          this.success = true;
+          this.deleting = false;
+          this.clearForm();
+        }))
+        .catch(() => {
+          this.error = true;
+          this.deleting = false;
+          this.clearForm();
+        });
     },
     validateFields() {
       this.$v.$touch();
-
       if (!this.$v.$invalid) {
         this.deleteArtifacts();
       }
@@ -153,22 +204,34 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-  .md-progress-bar {
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.md-progress-bar {
     position: absolute;
     top: 0;
     right: 0;
     left: 0;
+}
+
+.invalidFormValidation {
+  animation: shake 0.82s cubic-bezier(.36,.07,.19,.97) both;
+  transform: translate3d(0, 0, 0);
+  backface-visibility: hidden;
+  perspective: 1000px;
+}
+
+@keyframes shake {
+  10%, 90% {
+    transform: translate3d(-1px, 0, 0);
   }
-</style>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-  @import "~vue-material/dist/theme/engine"; // Import the theme engine
-
-  @include md-register-theme("default", (
-    primary: #706dff, // The primary color of your brand
-    accent: #6ea0e8 // The secondary color of your brand
-  ));
-  @import "~vue-material/dist/theme/all"; // Apply the theme
+  20%, 80% {
+    transform: translate3d(2px, 0, 0);
+  }
+  30%, 50%, 70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+  40%, 60% {
+    transform: translate3d(4px, 0, 0);
+  }
+}
 </style>
